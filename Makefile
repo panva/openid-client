87b05cd5f91722d04ec6ab6dc94ef25f4dcd243a
Filip Skokan <panva.ip@gmail.com>
2016-07-13 22:06:51 +0200

    more tests

--- Makefile
+++ Makefile
@@ -1,24 +1,20 @@
 NODE_VERSION = $(wordlist 1,1,$(subst ., ,$(subst v, ,$(shell node -v))))
 
-ifeq ($(shell echo ${NODE_VERSION}\<6 | bc), 1)
-	FLAGS = --harmony_destructuring
-endif
-
 TESTS = test/**/**/*.test.js test/**/*.test.js
 
 test:
-	node $(FLAGS) \
+	node \
 		./node_modules/.bin/_mocha \
 		$(TESTS)
 
 coverage:
-	node $(FLAGS) \
+	node \
 		./node_modules/.bin/istanbul cover \
 		./node_modules/.bin/_mocha \
 		$(TESTS)
 
 test-travis:
-	node $(FLAGS) \
+	node \
 		./node_modules/.bin/istanbul cover \
 		./node_modules/.bin/_mocha \
 		--report lcovonly \
---
+++ test/client/discover_client.test.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const Issuer = require('../../lib').Issuer;
+const expect = require('chai').expect;
+const nock = require('nock');
+const got = require('got');
+const fail = () => {
+  throw new Error('expected promise to be rejected');
+};
+const issuer = new Issuer();
+
+describe('Client#fromUri()', function () {
+  it('accepts and assigns the disovered metadata', function () {
+    nock('https://op.example.com')
+      .get('/client/identifier')
+      .reply(200, {
+        client_id: 'identifier',
+        client_secret: 'secure',
+      });
+
+    return issuer.Client.fromUri('https://op.example.com/client/identifier').then(function (client) {
+      expect(client).to.have.property('client_id', 'identifier');
+      expect(client).to.have.property('client_secret', 'secure');
+    });
+  });
+
+  it('is rejected with OpenIdConnectError upon oidc error', function () {
+    nock('https://op.example.com')
+      .get('/client/identifier')
+      .reply(500, {
+        error: 'server_error',
+        error_description: 'bad things are happening',
+      });
+
+    return issuer.Client.fromUri('https://op.example.com/client/identifier')
+      .then(fail, function (error) {
+        expect(error).to.have.property('message', 'server_error');
+      });
+  });
+
+  it('is rejected with when non 200 is returned', function () {
+    nock('https://op.example.com')
+      .get('/client/identifier')
+      .reply(500, 'Internal Server Error');
+
+    return issuer.Client.fromUri('https://op.example.com/client/identifier')
+      .then(fail, function (error) {
+        expect(error).to.be.an.instanceof(got.HTTPError);
+      });
+  });
+
+  it('is rejected with JSON.parse error upon invalid response', function () {
+    nock('https://op.example.com')
+      .get('/client/identifier')
+      .reply(200, '{"notavalid"}');
+
+    return issuer.Client.fromUri('https://op.example.com/client/identifier')
+      .then(fail, function (error) {
+        expect(error).to.be.an.instanceof(SyntaxError);
+        expect(error).to.have.property('message').matches(/Unexpected token/);
+      });
+  });
+});
