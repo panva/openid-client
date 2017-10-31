2aa7ef9270062547b8fbac5764d94544c62bb908
Filip Skokan <panva.ip@gmail.com>
2017-10-31 16:34:25 +0100

    default token auth to _post when _basic is not supported but _post is. closes #57

--- test/client/new_client.test.js
+++ test/client/new_client.test.js
@@ -71,4 +71,18 @@ describe('new Client()', function () {
     expect(client.metadata).to.have.property('metadata', 'foobar');
     expect(client.metadata).to.have.property('userinfo', 'foobar');
   });
+
+  context('dynamic registration defaults not supported by issuer', function () {
+    it('token_endpoint_auth_method vs. token_endpoint_auth_methods_supported', function () {
+      const issuer = new Issuer({
+        issuer: 'https://op.example.com',
+        token_endpoint_auth_methods_supported: ['client_secret_post', 'private_key_jwt'],
+      });
+      const client = new issuer.Client({
+        client_id: 'client',
+      });
+
+      expect(client.token_endpoint_auth_method).to.equal('client_secret_post');
+    });
+  });
 });
