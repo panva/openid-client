4891b5b078dade80099ef22e19b1f8b1c3179f0f
Filip Skokan <panva.ip@gmail.com>
2019-07-12 09:17:20 +0200

    fix: give AAD v2 organizations and consumers same treatment as common
    Closes #175

--- test/aad/aad.test.js
+++ test/aad/aad.test.js
@@ -3,38 +3,50 @@ const nock = require('nock');
 
 const { Issuer } = require('../../lib');
 
-const INPUTS = [
-  'https://login.microsoftonline.com/common/v2.0',
-  'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration',
-];
+const INPUTS = {
+  common: [
+    'https://login.microsoftonline.com/common/v2.0',
+    'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration',
+  ],
+  consumers: [
+    'https://login.microsoftonline.com/consumers/v2.0',
+    'https://login.microsoftonline.com/consumers/v2.0/.well-known/openid-configuration',
+  ],
+  organizations: [
+    'https://login.microsoftonline.com/organizations/v2.0',
+    'https://login.microsoftonline.com/organizations/v2.0/.well-known/openid-configuration',
+  ],
+};
 
 const idToken = 'eyJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJmb28iLCJhdWQiOiJmb28iLCJpYXQiOjEyMzQ1LCJleHAiOjEyMzQ1LCJpc3MiOiJodHRwczovL2xvZ2luLm1pY3Jvc29mdG9ubGluZS5jb20vZm9vL3YyLjAiLCJ0aWQiOiJmb28ifQ';
 const fail = () => { throw new Error('expected promise to be rejected'); };
 
 describe('Azure AD multi-tenant applications', () => {
-  INPUTS.forEach((input, i) => {
-    it(`changes the "iss" validation when Issuer is discovered ${i + 1}/${INPUTS.length}`, async () => {
-      nock('https://login.microsoftonline.com')
-        .get('/common/v2.0/.well-known/openid-configuration')
-        .reply(200, {
-          issuer: 'https://login.microsoftonline.com/{tenantid}/v2.0',
-        });
+  Object.entries(INPUTS).forEach(([bucket, inputs]) => {
+    inputs.forEach((input) => {
+      it(`changes the "iss" validation when Issuer is discovered (${input})`, async () => {
+        nock('https://login.microsoftonline.com')
+          .get(`/${bucket}/v2.0/.well-known/openid-configuration`)
+          .reply(200, {
+            issuer: 'https://login.microsoftonline.com/{tenantid}/v2.0',
+          });
 
-      const aad = await Issuer.discover(input);
-      const client = new aad.Client({ client_id: 'foo' });
-      return client.validateIdToken(idToken).then(fail).catch((err) => {
-        expect(err.message).to.match(/^id_token expired, now \d+, exp 12345$/);
+        const aad = await Issuer.discover(input);
+        const client = new aad.Client({ client_id: 'foo' });
+        return client.validateIdToken(idToken).then(fail).catch((err) => {
+          expect(err.message).to.match(/^id_token expired, now \d+, exp 12345$/);
+        });
       });
     });
-  });
 
-  it('no changes to "iss" validation when Issuer is constructed', async () => {
-    const aad = new Issuer({
-      issuer: 'https://login.microsoftonline.com/{tenantid}/v2.0',
-    });
-    const client = new aad.Client({ client_id: 'foo' });
-    return client.validateIdToken(idToken).then(fail).catch((err) => {
-      expect(err.message).to.eql('unexpected iss value, expected https://login.microsoftonline.com/{tenantid}/v2.0, got: https://login.microsoftonline.com/foo/v2.0');
+    it('no changes to "iss" validation when Issuer is constructed', async () => {
+      const aad = new Issuer({
+        issuer: 'https://login.microsoftonline.com/{tenantid}/v2.0',
+      });
+      const client = new aad.Client({ client_id: 'foo' });
+      return client.validateIdToken(idToken).then(fail).catch((err) => {
+        expect(err.message).to.eql('unexpected iss value, expected https://login.microsoftonline.com/{tenantid}/v2.0, got: https://login.microsoftonline.com/foo/v2.0');
+      });
     });
   });
 });
