008c35f2ae94f59ee4b56c1233f54d0d7e327cb2
Filip Skokan <panva.ip@gmail.com>
2020-01-01 21:26:04 +0100

    test: update asserted error message

--- test/client/client_instance.test.js
+++ test/client/client_instance.test.js
@@ -2733,7 +2733,7 @@ describe('Client', () => {
 
             return this.client.unpackAggregatedClaims(userinfo)
               .then(fail, (err) => {
-                expect(err).to.have.property('message', 'failed to validate the aggregated JWT (JWTClaimInvalid: token is expired)');
+                expect(err).to.have.property('message', 'failed to validate the aggregated JWT (JWTClaimInvalid: "exp" claim timestamp check failed)');
               });
           });
         });
