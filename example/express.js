b16f46d6141a801c39d14e22392429664a8f3963
Andrew McWatters <mcwattersandrew@gmail.com>
2023-06-28 20:47:58 -0700

    Add passport

--- example/express.js
+++ example/express.js
@@ -1,6 +1,7 @@
 (async () => {
   const express = require('express')
-  const { Issuer } = await import('openid-client')
+  const { Issuer, Strategy: OpenIDConnectStrategy } = await import('openid-client')
+  const passport = require('passport')
   const app = express()
   const port = 3001
 
@@ -14,9 +15,25 @@
     // id_token_signed_response_alg (default "RS256")
   }) // => Client
 
+  passport.use(new OpenIDConnectStrategy({
+      client
+    },
+    function(tokenset, done) {
+      return done(null, tokenset.claims())
+    }
+  ))
+
   app.get('/', (req, res) => {
     res.send('Hello World!')
   })
+
+  app.get('/login', passport.authenticate('localhost'))
+
+  app.get('/cb',
+    passport.authenticate('localhost', { failureRedirect: '/login', failureMessage: true }),
+    function(req, res) {
+      res.redirect('/')
+    })
 
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
