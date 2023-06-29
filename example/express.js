6801c5630d1f4b08df960d94cfe0176d9ae8805c
Andrew McWatters <mcwattersandrew@gmail.com>
2023-06-28 20:36:22 -0700

    Quick start
    https://github.com/panva/node-openid-client/blob/v5.4.2/README.md#quick-start

--- example/express.js
+++ example/express.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3001
+(async () => {
+  const express = require('express')
+  const { Issuer } = await import('openid-client')
+  const app = express()
+  const port = 3001
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  const oidcIssuer = await Issuer.discover('http://localhost:3000')
+  console.log('Discovered issuer %s %O', oidcIssuer.issuer, oidcIssuer.metadata)
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+})()
