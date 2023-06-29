18509a26b612ff35d1ff2a2289a6f44ead218f7c
Andrew McWatters <mcwattersandrew@gmail.com>
2023-06-28 20:39:30 -0700

    Implicit ID Token Flow
    https://github.com/panva/node-openid-client/blob/v5.4.2/README.md#implicit-id-token-flow

--- example/express.js
+++ example/express.js
@@ -7,6 +7,13 @@
   const oidcIssuer = await Issuer.discover('http://localhost:3000')
   console.log('Discovered issuer %s %O', oidcIssuer.issuer, oidcIssuer.metadata)
 
+  const client = new oidcIssuer.Client({
+    client_id: 'oidcCLIENT',
+    redirect_uris: ['http://localhost:3001/cb'],
+    response_types: ['id_token'],
+    // id_token_signed_response_alg (default "RS256")
+  }) // => Client
+
   app.get('/', (req, res) => {
     res.send('Hello World!')
   })
