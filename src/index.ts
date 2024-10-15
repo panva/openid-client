8955583d6862393d67d323628a758093f4eb52a6
Filip Skokan <panva.ip@gmail.com>
2024-10-16 00:06:26 +0200

    chore(release): 6.0.0

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ if (
   typeof navigator === 'undefined' || !navigator.userAgent?.startsWith?.('Mozilla/5.0 ') // prettier-ignore
 ) {
   const NAME = 'openid-client'
-  const VERSION = 'v5.7.0'
+  const VERSION = 'v6.0.0'
   USER_AGENT = `${NAME}/${VERSION}`
   headers = { 'user-agent': USER_AGENT }
 }
