02733578fbf980aa6d85695ab810fec65188acf0
Filip Skokan <panva.ip@gmail.com>
2025-11-03 20:54:49 +0100

    chore: bump packages

--- conformance/ava.config.ts
+++ conformance/ava.config.ts
@@ -185,7 +185,7 @@ export default async () => {
 
   if (needsClientCertificate(PLAN_NAME, variant)) {
     const { generate } = await import('selfsigned')
-    const selfsigned = generate(undefined, { keySize: 2048 })
+    const selfsigned = generate()
     clientConfig.certificate = selfsigned.cert
     mtls = {
       cert: selfsigned.cert,
